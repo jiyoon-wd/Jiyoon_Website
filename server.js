987b995c269c12f701f43d7c202a3d083636c42c
Kkomaengs <jylpo121@gmail.com>
2022-08-14 23:56:59 -0400

    update

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path')
 const PORT = process.env.PORT || 4000
 
 express()
-  .use(express.static(path.join(__dirname)))
-  .set('views', path.join(__dirname))
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
